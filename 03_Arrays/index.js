ea23ec140e2cf7ac47f0d3e7b4ff93618dfc260b
jharry444 <jharrison1@qa.com>
2023-11-20 16:08:12 +0000

    added arrays example

---
+++ 03_Arrays/index.js
@@ -0,0 +1,9 @@
+// create an array with three names
+const arr = ['Jordan', "Cameron", "Elliot"];
+// logs out the whole array
+console.log(arr);
+
+// logs out the first element
+console.log(arr[0]);
+// changes the first element
+arr[0] = "Jordan Harrison"
