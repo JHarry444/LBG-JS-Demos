defa77779902ee914a68e116d82b583634183a1e
jharry444 <jharrison1@qa.com>
2023-11-20 11:23:15 +0000

    init commit

---
+++ 01_Types/index.js
@@ -0,0 +1,38 @@
+let age = 29;
+
+//log
+console.log(age);
+// this just USES the value of age
+console.log(age + 1);
+
+console.log(age);
+// need to use '=' to CHANGE the value of age
+age = 30;
+
+console.log(age);
+
+let myName = 'Jordan';
+
+console.log(myName);
+
+myName = "Joseph";
+
+console.log(myName);
+
+let muppet = false;
+
+console.log(muppet);
+
+muppet = true;
+
+console.log(muppet);
+
+let demo;
+// undefined as it NEVER HAD A VALUE
+console.log(demo);
+
+demo = null;
+
+console.log(demo);
+// use typeof to interrogate what type a variable is
+console.log(typeof muppet);
