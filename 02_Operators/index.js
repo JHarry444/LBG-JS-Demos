f4ba70265f66e9740215f424fe7cd65279848626
jharry444 <jharrison1@qa.com>
2023-11-20 14:42:54 +0000

    operators example

---
+++ 02_Operators/index.js
@@ -0,0 +1,63 @@
+let num = 4;
+
+console.log(4 + 4);
+console.log(4 - 4);
+console.log(4 * 4);
+console.log(4 / 4);
+// % -> divide and find remainder
+console.log(10 % 4);
+console.log(4 ** 4);
+
+let num2 = 12;
+
+console.log(num2 == 24);
+console.log(num2);
+
+console.log('1' == 1 == true);
+console.log('1' === 1);
+
+console.log("Jordan " + "Harrison");
+
+console.log(true + true);
+
+let concat = "Text " + 12;
+
+console.log(concat);
+console.log(typeof concat);
+
+console.log("12" + "8");
+console.log(typeof ("12" + "8"));
+
+
+console.log("12" - "8");
+console.log(typeof ("12" - "8"));
+
+console.log("12" - "test");
+console.log(typeof ("12" - "test"));
+
+
+let x = 12;
+
+x = x + 1;
+
+console.log(x);
+
+x += 1;
+
+console.log(x);
+
+x++;
+
+console.log(x);
+
+x--;
+
+console.log(x);
+
+console.log(true);
+console.log(false);
+
+console.log(!true);
+console.log(!false);
+
+console.log(30 % 2 !== 0);
