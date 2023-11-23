0c2c8df3da7b26b8bd4de6f4230b66c56a7ef25f
jharry444 <jharrison1@qa.com>
2023-11-23 13:53:12 +0000

    added DOM example

---
+++ 08_DOM_Manipulation/index.js
@@ -0,0 +1,21 @@
+const newParagraph = document.createElement("p");
+
+newParagraph.textContent = "Hold your horses mikey"
+
+document.body.appendChild(newParagraph);
+
+
+const newHeading = document.createElement("h1");
+
+
+document.body.appendChild(newHeading);
+
+newHeading.textContent = "First heading";
+
+newHeading.style.color = "red";
+
+document.body.className = 'dark'
+
+document.body.className = 'light'
+
+document.body.appendChild(document.createElement("input"));
