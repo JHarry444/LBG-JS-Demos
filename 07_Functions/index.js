3e54c3ea8c9442fc349d8536f11163ec38273a22
jharry444 <jharrison1@qa.com>
2023-11-23 11:42:24 +0000

    diet functions

--- 07_Functions/index.js
+++ 07_Functions/index.js
@@ -22,7 +22,7 @@ function fizzBuzz(num) {
         console.log("FizzBuzz");
     } else if (num % 3 === 0) {
         console.log("Fizz");
-    } else if ( num % 5 === 0) {
+    } else if (num % 5 === 0) {
         console.log("Buzz");
     } else {
         console.log(num);
@@ -33,16 +33,39 @@ function sum(num, num2) {
     return num + num2; // the OUTPUT of this function is num + num2
 }
 
-function product(a, b) {
-    console.log(a * b);
+// automatically has a 'return' cos there's no {}
+// only works with functions that are a single line
+const product = (a, b) => a * b;
+
+// this one has {} so YOU have to add the 'return'
+const divide = (a, b) => {
+    return a / b;
 }
 
+const subtract = function (a, b) {
+    return a - b;
+}
+
+console.log(typeof subtract); // function variable
+
+console.log(subtract(12, 6));
+// can skip the parenthesis around the parameters if there's only one
+const print = toPrint => console.log(toPrint);
+
+print("YO");
+
 const mySum = sum(12, 24);
+
+console.log("SUM:", mySum);
+
+const myProduct = product(12, 24);
+
+console.log("Product:", myProduct);
 
 // write functions to; subtract, multiply and divide 2 numbers
 
 
-let nums = [12,545,43,35,35,42];
+let nums = [12, 545, 43, 35, 35, 42];
 
 function arrayMax(array) {
     let max = 0;
