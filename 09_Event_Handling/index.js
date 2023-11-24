e0938758a543059ec10a4654b89e93c5978bc350
jharry444 <jharrison1@qa.com>
2023-11-24 14:12:41 +0000

    more event handling

--- 09_Event_Handling/index.js
+++ 09_Event_Handling/index.js
@@ -1,3 +1,15 @@
-function hello() {
-    alert("Hello!");
+function hello(text) {
+    alert(text);
+}
+
+function toggle() {
+    if (document.body.className === 'light')
+        document.body.className = "dark";
+    else
+        document.body.className = "light";
+}
+
+function addText() {
+    const myP = document.getElementById("myP");
+    myP.textContent = "EVENT HANDLING!!1!!";
 }
